739acdecbf27396553c4244dce808cb19f1a18bc
Gabe <gabe.kanzelmeyer@gmail.com>
2026-09-21 07:19:14 -0700

    adding commit.sh file

---
+++ commit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Require a commit message
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 \"commit message\""
+    exit 1
+fi
+
+COMMIT_MESSAGE="$*"
+USERNAME="gabekanzelmeyer"
+PASSWORD_FILE="/home/gabe/.github_pw"
+
+# Read password from file
+if [ ! -f "$PASSWORD_FILE" ]; then
+    echo "Password file not found: $PASSWORD_FILE"
+    exit 1
+fi
+
+PASSWORD=$(cat "$PASSWORD_FILE")
+
+# Make sure the password isn't empty
+if [ -z "$PASSWORD" ]; then
+    echo "Password file is empty"
+    exit 1
+fi
+
+echo "Adding files..."
+git add -A || exit 1
+
+echo "Committing..."
+git commit -m "$COMMIT_MESSAGE" || exit 1
+
+echo "Pushing..."
+git push || exit 1
+
+echo "Done."
